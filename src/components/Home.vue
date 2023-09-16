ff9d6f2d9ed046516831b77f9811795288ca8a76
panchomb <francisco.magot@utec.edu.pe>
2023-09-16 22:42:49 +0200

    add: initial home page

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h1>Welcome to IE Bank</h1>
+          <p>This is the home page of IE Bank Platform.</p>
+          <button @click="redirectToAccounts" class="btn btn-success btn-lg">
+            Go to Accounts
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToAccounts() {
+      this.$router.push('/accounts');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
